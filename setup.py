1261a5ee4e22a6c04ad8b7eb9b63e9ee18a785f7
Luka Antonyshyn <15la@caslab.queensu.ca>
2019-11-07 12:08:09 -0500

    Add setup file for dependencies

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='Leaf-Identification',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/QMIND-Team/Leaf-Identification',
+      install_requires=[
+          'keras',
+          'tensorflow',
+          'numpy',
+          'scikit-learn',
+          'matplotlib',
+          'opencv-python',
+      ])
